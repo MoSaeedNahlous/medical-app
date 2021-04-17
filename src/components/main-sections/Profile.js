722902636298b8f93759200ba30b35703d563603
MoSaeedNahlous <jay.haji041@gmail.com>
2021-04-17 21:57:07 +0300

    حقخبهمث

--- src/components/main-sections/Profile.js
+++ src/components/main-sections/Profile.js
@@ -1,17 +1,58 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
+import { UserGlobalContext } from '../../context/userContext/UserState'
 const Profile = () => {
+
+    const context = useContext(UserGlobalContext)
+
+    const { user,LoadUser} = context
+
+    useEffect(() => {
+       LoadUser()
+    }, [])
     
     return (
         <Fragment>
             <Header />
-            <div>
+            <div style={{padding:'4%'}}>
                 <h1>My Profile</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi perferendis, illum officiis sapiente doloremque ea illo
-                eius quaerat vero exercitationem
-                deserunt harum cupiditate sit! Libero non amet iste enim alias?</p>
+                <br />
+                <br />
+                <h3>My info</h3>
+                <br/>
+
+                <div>
+                    <div>
+                    <h4>First Name : {user.firstName}</h4>  
+                    
+                </div><br/>
+
+                <div>
+                    <h4>Last Name : {user.lastName}</h4>
+                    
+                </div><br/>
+
+                <div>
+                    <h4>Email : {user.email}</h4>
+                    
+                </div><br/>
+                     
+                <div>
+                    <h4>Mobile Number : {user.mobileNumber}</h4>
+                    
+                </div><br/>
+                
+                <div>
+                    <h4>Gender : {(user.gender)?("male"):("female")}</h4>
+                        
+                </div><br/>
+                
+                <div>
+                    <h4>BirthDate : {user.birthDay}</h4>
+                    
+                </div><br/>
+                </div>
             </div>
             <Footer />
         </Fragment>
