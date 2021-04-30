3db80402a237dc1dbaee13e709677203d5b731be
MoSaeedNahlous <jay.haji041@gmail.com>
2021-04-30 21:16:38 +0300

    ..

--- src/components/layout/admin/UsersPanel.js
+++ src/components/layout/admin/UsersPanel.js
@@ -27,6 +27,7 @@ const UsersPanel = () => {
   useEffect(() => {
     ClearCurrentUser();
     GetAllUsers();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
     
     useEffect(() => {
